ad487f3c48d21df593aeb2cb3a0d020b75f881bd
JuanitaCampos1505 <88303473+JuanitaCampos1505@users.noreply.github.com>
2021-11-23 15:25:07 -0500

    ver clientes

--- src/app/components/clientes-list/clientes-list.component.ts
+++ src/app/components/clientes-list/clientes-list.component.ts
@@ -5,6 +5,8 @@ import { Empleado } from 'src/app/models/empleado';
 import { EmpleadoService } from 'src/app/services/empleado.service';
 import { PersonaExterna } from 'src/app/models/persona-externa';
 import { PersonaExternaService } from 'src/app/services/persona-externa.service';
+import { Empresa } from 'src/app/models/empresa';
+import { EmpresaService } from 'src/app/services/empresa.service';
 
 @Component({
   selector: 'app-clientes-list',
@@ -13,13 +15,86 @@ import { PersonaExternaService } from 'src/app/services/persona-externa.service'
 })
 export class ClientesListComponent implements OnInit {
   empleados:Empleado[];
-  personaExterna:PersonaExterna[];
-  constructor(private empleadoService:EmpleadoService, private router:Router, private activate:ActivatedRoute) { 
+  personas:PersonaExterna[];
+  empresas:Empresa[];
+  title:string;
+  constructor(private empresaService:EmpresaService, 
+    private empleadoService:EmpleadoService, 
+    private personaExternaService:PersonaExternaService,  
+    private router:Router, private activate:ActivatedRoute) { 
     this.empleados = [];
-    this.personaExterna = [];
+    this.personas = [];
+    this.empresas = [];
+    this.title = 'Lista de clientes';
   }
 
   ngOnInit(): void {
+    this.empresaService.getEmpresas().subscribe(
+      empresas => {
+        this.empresas = empresas;
+      }
+    );
   }
 
+  loadEmpresas():void{
+    this.personas = [];
+    this.empleados = [];
+    this.empresaService.getEmpresas().subscribe(
+      empresas => {
+        this.empresas = empresas;
+      }
+    );
+  }
+
+  loadPersonas():void{
+    this.empresas = [];
+    this.empleados = [];
+    this.personaExternaService.getPersonasExternas().subscribe(
+      personas => {
+        this.personas = personas;
+      }
+    );
+  }
+
+  loadEmpleados():void{
+    this.personas = [];
+    this.empresas = [];
+    this.empleadoService.getEmpleados().subscribe(
+      empleados => {
+        this.empleados = empleados;
+        console.log(this.empleados);
+      }
+    );
+  }
+
+  deleteEmpleado(empleado:Empleado):void{
+    Swal.fire(
+      {
+        title: 'Estás seguro, parce?',
+        text: 'Esto no tiene vuelta atrás',
+        icon: 'warning',
+        showCancelButton: true,
+        cancelButtonText: 'Uy zonas',
+        confirmButtonText: 'Sisas'
+      }
+    ).then(
+      (result) => {
+        if (result.isConfirmed) {
+          this.empleadoService.delete(empleado.id!).subscribe(
+            response => {
+              this.empleados = this.empleados.filter(emp => emp != empleado);
+              Swal.fire(
+                {
+                  title: 'Eres una chimba',
+                  text: 'Borraste un empleado ome',
+                  icon: 'success',
+                  confirmButtonText: 'Melo'
+                }
+              );
+            }
+          );
+        }
+      }
+    );
+  }
 }
---
+++ src/app/services/persona.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { Persona } from '../models/persona';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+  private urlEndPoint = 'http://localhost:8081/api/personas/';
+  private header = new HttpHeaders({
+    'Content-type': 'application/json'
+  });
+  constructor(private http:HttpClient) { }
+
+  delete(id:string):Observable<Persona>{
+    return this.http.delete<Persona>(
+      this.urlEndPoint + id,
+      {
+        headers: this.header
+      }
+    );
+  }
+}
