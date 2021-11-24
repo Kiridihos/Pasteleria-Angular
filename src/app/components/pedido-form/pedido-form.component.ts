6008ed6072033a85d237b96c286c9073c6ac715b
kiridihos <47702142+Kiridihos@users.noreply.github.com>
2021-11-23 20:17:05 -0500

    orden

--- src/app/components/pedido-form/pedido-form.component.ts
+++ src/app/components/pedido-form/pedido-form.component.ts
@@ -1,4 +1,10 @@
+import { PedidoService } from './../../services/pedido.service';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Empleado } from 'src/app/models/empleado';
+import { EmpleadoService } from 'src/app/services/empleado.service';
+import { Pedido } from 'src/app/models/pedido';
 
 @Component({
   selector: 'app-pedido-form',
@@ -6,10 +12,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pedido-form.component.css']
 })
 export class PedidoFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  pedido:Pedido;
+  title:string;
+  empleado:Empleado[];
+  constructor(private PedidoService:PedidoService, private empleadoService:EmpleadoService, private router:Router, private activate:ActivatedRoute) { 
+    this.pedido = new Pedido();
+    this.empleado = [];
+    this.title = '';
   }
 
+  ngOnInit(): void {
+    this.cargarPedido();
+    this.getEmpleado();
+  }
+
+  getEmpleado():void{
+    this.empleadoService.getEmpleados().subscribe(
+      empleado => this.empleado = empleado
+    );
+  }
+
+  create():void{
+    this.PedidoService.create(this.pedido).subscribe(
+      response => {
+        this.router.navigate(['/']);
+        Swal.fire(
+          {
+            title: 'Eres una chimba',
+            text: 'Creaste un orden ome',
+            icon: 'success',
+            confirmButtonText: 'Melo'
+          }
+        );
+      }
+    );
+  }  
+  cargarPedido():void{
+    this.activate.params.subscribe(
+      params => {
+        let id = params['id'];
+        if (id) {
+          this.PedidoService.getPedido(id).subscribe(
+            pedido => this.pedido = pedido
+          );
+          this.title = 'Editar Orden';
+        }
+        else {
+          this.title = 'Registrar Orden';
+        }
+      }
+    );
+  }
 }
--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { Pedido } from '../models/pedido';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidoService {
+  private urlEndPoint = 'http://localhost:8081/api/pedidos/';
+  private header = new HttpHeaders({
+    'Content-type': 'application/json'
+  });
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getPedidos(): Observable<Pedido[]>{
+    return this.http.get(this.urlEndPoint).pipe(
+      map((response) => response as Pedido[])
+    );
+  }
+
+  getPedido(id:string): Observable<Pedido>{
+    return this.http.get<Pedido>(
+      this.urlEndPoint + id
+    );
+  }
+
+  create(pedido:Pedido): Observable<Pedido>{
+    return this.http.post<Pedido>(
+      this.urlEndPoint,
+      pedido,
+      {
+        headers: this.header
+      }
+    );
+  }
+
+  delete(id:string): Observable<Pedido>{
+    return this.http.delete<Pedido>(
+      this.urlEndPoint + id,
+      {
+        headers: this.header
+      }
+    );
+  }
 }
