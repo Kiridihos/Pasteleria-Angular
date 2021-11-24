4c5320ebbb57e2b5e5d1ba1f03ddffaf5285b9db
kiridihos <47702142+Kiridihos@users.noreply.github.com>
2021-11-23 22:12:09 -0500

    se crea servicio de pastel

--- src/app/services/pastel.service.ts
+++ src/app/services/pastel.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Pastel } from '../models/pastel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PastelService {
+  private urlEndPoint = 'http://localhost:8081/api/pasteles/';
+  private header = new HttpHeaders({
+    'Content-type': 'application/json'
+  });
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getPasteles(): Observable<Pastel[]>{
+    return this.http.get(this.urlEndPoint).pipe(
+      map((response) => response as Pastel[])
+    );
+  }
+
+  getPastel(id:string): Observable<Pastel>{
+    return this.http.get<Pastel>(
+      this.urlEndPoint + id
+    );
+  }
+
+  create(pastel:Pastel): Observable<Pastel>{
+    return this.http.post<Pastel>(
+      this.urlEndPoint,
+      pastel,
+      {
+        headers: this.header
+      }
+    );
+  }
+
+  delete(id:string): Observable<Pastel>{
+    return this.http.delete<Pastel>(
+      this.urlEndPoint + id,
+      {
+        headers: this.header
+      }
+    );
+  }
 }
